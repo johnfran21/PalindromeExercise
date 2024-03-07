73aac25ca8897afbf7c576aa0188ac7c5eca0fd7
John Franzese <johnfranzese21@gmail.com>
2024-03-07 16:58:06 -0600

    Test Driven Development Part 2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+        public bool IsAPalindrome(string originalWord)
+		{
+			string reversedWord = "";
+            for (int i = originalWord.Length - 1; i >= 0; i--)
+			{
+                reversedWord += originalWord[i];
+			}
+
+			return originalWord.ToLower() == reversedWord.ToLower();
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,29 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("level", true)]
+        [InlineData("goodbye", false)]
+        [InlineData("radar", true)]
+
+
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            WordSmith instance = new WordSmith();
+
+            //Act
+            bool actual = instance.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
